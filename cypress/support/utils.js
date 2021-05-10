4e6e51eb1c20bc06f696e9f056be0683dee244f9
Renato Santana de Oliveira <renato.saoli@gmail.com>
2021-05-10 13:59:34 -0300

    melhorando funcao screenshot tests

--- cypress/support/utils.js
+++ cypress/support/utils.js
@@ -23,9 +23,15 @@ class Util {
 
     screenshot() {
         const dateFull = new Date();
-        const dateCurrent = dateFull.getUTCDate() + '-' + (dateFull.getUTCMonth() + 1) + '-' + dateFull.getFullYear()
-        const horaCurrent = dateFull.getHours() + '' + dateFull.getMinutes() + '' + dateFull.getSeconds()
-        var dateTimeCurrent = dateCurrent + '_' + horaCurrent
+
+        var d = dateFull.getDate()
+        var m = dateFull.getMonth() + 1
+        var y = dateFull.getFullYear()
+        var h = dateFull.getHours()
+        var m = dateFull.getMinutes()
+        var s = dateFull.getSeconds()
+
+        var dateTimeCurrent = '' + y + '-' + (m<=9 ? '0' + m : m) + '-' + (d <= 9 ? '0' + d : d) + ' ' + h + ':' + m + s;
 
         cy.screenshot(dateTimeCurrent)
     }
